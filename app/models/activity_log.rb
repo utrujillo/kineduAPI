8182962b17d96322170113464f81f911eab81686
Uziel Trujillo <uziel.trujillo@gmail.com>
2020-07-24 16:13:39 -0500

    Agregando restricciones para el salvado de las actividades
    Notas:
    - Se verifica que la actividad no pueda ser finalizada en un tiempo previo a la fecha y hora de inicio
    - Se realiza el calculo de las horas de la actividad de forma automatica antes de guardar
    	modified:   app/models/activity_log.rb

--- app/models/activity_log.rb
+++ app/models/activity_log.rb
@@ -8,5 +8,22 @@ class ActivityLog < ApplicationRecord
 	validates :activity_id, presence: true, numericality: { only_integer: true }
 	validates :baby_id, presence: true, numericality: { only_integer: true }
 	validates :assistant_id, presence: true, numericality: { only_integer: true }
-	validates :start_time, presence: true
+  validates :start_time, presence: true
+
+  # Acciones
+  validate do
+    begin
+      time_difference = ((self.stop_time - self.start_time) / 1.hour).round
+      if time_difference < 0
+        self.errors.add(:base, "La actividad no puede ser terminada en un tiempo previo a la fecha y hora de inicio")
+      end
+    rescue => e
+      self.errors.add(:base, "La actividad no puede ser terminada en un tiempo previo a la fecha y hora de inicio")
+    end
+  end
+
+  before_save do
+    time_difference = ((self.stop_time - self.start_time) / 1.hour).round
+    self.duration = time_difference > 0 ? time_difference : 0
+  end
 end
