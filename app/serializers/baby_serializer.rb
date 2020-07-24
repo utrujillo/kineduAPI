6585e4873429fd7da747873aa7709328081c9a6f
Uziel Trujillo <uziel.trujillo@gmail.com>
2020-07-24 13:07:56 -0500

    Agregando serializer para retorno de informacion requisitado 	new file:   app/serializers/baby_serializer.rb

---
+++ app/serializers/baby_serializer.rb
@@ -0,0 +1,11 @@
+class BabySerializer < ActiveModel::Serializer
+  attributes :name, :age_in_months, :mother_name, :father_name, :address, :phone
+
+  def age_in_months
+    calculate_months( object.birthday.to_date, Time.now.strftime("%Y-%m-%d").to_date )
+  end
+
+  def calculate_months(date1, date2)
+    (date2.year * 12 + date2.month) - (date1.year * 12 + date1.month)
+  end
+end
